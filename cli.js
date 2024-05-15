7ac68b2c53e8924a5a92674e9f6df172f408c4aa
Robin Millette <robin@millette.info>
2024-05-15 12:55:54 -0400

    Use meow.

--- cli.js
+++ cli.js
@@ -1,5 +1,22 @@
+// node
+import meow from "meow"
+
 // self
 import { fetchAll } from "./index.js"
 
-const [benevity, canadaHelps, nooe, mycharityfund, charitableimpact] = await fetchAll("Jacques de Champlain")
+const cli = meow(`
+  Usage
+    $ foo <input>
+
+  Options
+    --rainbow, -r  Include a rainbow
+
+  Examples
+    $ foo unicorns --rainbow
+    🌈 unicorns 🌈
+  `, {
+  importMeta: import.meta,
+})
+
+const [benevity, canadaHelps, nooe, mycharityfund, charitableimpact] = await fetchAll(cli.input[0])
 console.log(JSON.stringify({ benevity, canadaHelps, nooe, mycharityfund, charitableimpact }, null, 2))
