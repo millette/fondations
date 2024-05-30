098258e386a0d3ec1a3bcf15e9184faac08b88f1
Robin Millette <robin@millette.info>
2024-05-30 11:07:59 -0400

    Use constants.

--- constants.js
+++ constants.js
@@ -1,1 +1,8 @@
-export const LIMIT = 1
+export const LIMIT = 10
+export const MYCHARITYFUND_ORIG_FILENAME = "mycharityfund-all.json"
+export const MYCHARITYFUND_FILENAME = "mycharityfund-transliterate.json"
+export const MYCHARITYFUND_BASE = "https://mycharityfund.ca/api/website/"
+export const NOOE_BASE = "https://api.nooe.org/api/"
+export const CHARITABLEIMPACT_BASE = "https://api.charitableimpact.com/"
+export const CANADAHELPS_BASE = "https://www.canadahelps.org/site/api/charity-search/"
+export const BENEVITY_BASE = "https://causes.benevity.org/"
