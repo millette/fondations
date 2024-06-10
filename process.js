1a2b8c93e6ca74e9b9edf42e3aa8bb41dfd8a7a2
Robin Millette <robin@millette.info>
2024-06-10 15:23:50 -0400

    Remove exrtaneous files.

--- process.js
+++
@@ -1,6 +0,0 @@
-// self
-import processFile from "./lib/mycharityfund-process.js"
-
-const now = Date.now()
-const { json } = await processFile()
-console.log("Got", json.length, "entries in", Math.round((Date.now() - now) / 1000), "s")
